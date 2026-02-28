75906aebbd63018ed95f5d4e602a1a9a7553b9a3
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2026-02-28 18:19:53 +0100

    gegner, weakspots und portale

---
+++ src/enemies/weakspot_manager.gd
@@ -0,0 +1,34 @@
+extends Node3D
+
+
+@export var enemy: CharacterBody3D
+@export var weakspot_interval: float = 5.0
+@export var spawn_radius: float = 0.5
+@export var spawn_time_randomness: float = 3.0
+
+
+var next_time: float
+var time: float = 0.0
+var weakspot: PackedScene = preload("res://src/enemies/weakspot.tscn")
+
+
+func _ready() -> void:
+	next_time = weakspot_interval + randf_range(-spawn_time_randomness, spawn_time_randomness)
+
+
+func _process(delta: float) -> void:
+	time += delta
+	
+	if time >= next_time:
+		next_time = weakspot_interval + randf_range(-spawn_time_randomness, spawn_time_randomness)
+		time = 0.0
+		_spawn_weakspot()
+
+
+func _spawn_weakspot() -> void:
+	var weakspot_scene: WeakSpot = weakspot.instantiate()
+	
+	weakspot_scene.hit.connect(enemy.take_damage)
+	weakspot_scene.position = Vector3(randf_range(-spawn_radius, spawn_radius), randf_range(-spawn_radius, spawn_radius), 0.0)
+	self.add_child(weakspot_scene)
+	
